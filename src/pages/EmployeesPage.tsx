387979b25326cb000e7a29a01fd05e70b7ad56f3
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-01-21 21:24:21 +0100

    improve style and accessibility

--- src/pages/EmployeesPage.tsx
+++ src/pages/EmployeesPage.tsx
@@ -7,24 +7,32 @@ export function EmployeesPage() {
 
     const inputRef = useRef<HTMLInputElement | null>(null);
 
-    console.log("Render - employees:", employees);
-
     const handleAdd = () => {
         if (!inputRef.current) {
             console.error('InputRef is NULL');
         } else {
-            const fullname = inputRef.current.value;
-            console.log("Before add:", employees);
-            addEmployee(fullname);
-            console.log("After add:", employees);
+            addEmployee(inputRef.current.value);
+            inputRef.current.value = "";
         }
     };
 
     return (
         <div>
-            <h1 className="text-xl font-medium uppercase">Employees</h1>
-            <input type="text" className="border rounded-md" ref={inputRef} />
-            <button onClick={handleAdd}>Add Employee</button>
+            <h1 className="text-xl font-medium uppercase text-center mb-2">Employees List</h1>
+            <div className="flex flex-col gap-1.5 w-1/2 mx-auto">
+                <input
+                    type="text"
+                    className="border rounded-md px-2 py-1"
+                    ref={inputRef}
+                    onKeyDown={(e) => e.key === 'Enter' && handleAdd()}
+                />
+                <button
+                    onClick={handleAdd}
+                    className="border border-gray-600 rounded-md bg-blue-500 hover:bg-blue-400 cursor-pointer text-white px-2 py-1"
+                >
+                    Add Employee
+                </button>
+            </div>
             <ul>
                 {employees.map((employee) => (
                     <li key={employee.id}>{employee.fullname}</li>
