b73b20fb1baf72ae95fdade4fc88aca290447d06
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2026-01-25 13:09:26 +0100

    add admin authentication with password prompt and navigation

--- src/layouts/AdminLayout.tsx
+++ src/layouts/AdminLayout.tsx
@@ -1,9 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { useRef } from "react";
+import { Navigate, Outlet } from "react-router-dom";
+import { store } from "../store/store";
 
 export function AdminLayout() {
+    const isAdminAuthenticated = store(s => s.isAdminAuthenticated);
+    const authenticateAdmin = store(s => s.authenticateAdmin);
+    const hasPrompted = useRef(false);
+    const hasAlerted = useRef(false);
+
+    if (!isAdminAuthenticated) {
+        if (!hasPrompted.current) {
+            hasPrompted.current = true;
+            const inputPassword = prompt('Inserisci la password Admin');
+            if (authenticateAdmin(inputPassword)) {
+                return null;
+            }
+        }
+        if (!hasAlerted.current) {
+            hasAlerted.current = true;
+            alert("Password sbagliata!");
+        }
+        return <Navigate to='/kiosk' />;
+    }
+
     return (
         <>
-            <h2 className="text-xl font-medium">Admin layoy</h2>
+            <h2 className="text-xl font-medium">Admin layout</h2>
             <Outlet />
         </>
     )
